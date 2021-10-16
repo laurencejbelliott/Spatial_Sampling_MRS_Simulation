576be8056b233783df2b9c73de8832bb76d451e6
Laurence Roberts-Elliott <laurencejbelliott@gmail.com>
2021-10-16 16:42:43 +0100

    Grid BG colour changed to distinguish from sampled cells
    Use MESA visualisation to chart performance metrics. See if MESA can support continuous time, as opposed to running in discrete time intervals.

--- mesa_spatial_sampling_MRS/model.py
+++ mesa_spatial_sampling_MRS/model.py
@@ -3,6 +3,7 @@ import os
 import glob
 import random
 
+import mesa
 from mesa import Model, Agent
 from mesa.time import SimultaneousActivation
 from mesa.space import MultiGrid
@@ -11,12 +12,10 @@ import numpy as np
 from astar_python import Astar
 from gaussian import makeGaussian
 
-# TODO: Comment code where necessary for readability
-
 
 class Robot(Agent):
     def __init__(self, pos, model):
-        # Inherits pos(ition) and model attributes from Agent superclass
+        # Inherits position and model attributes from Agent superclass
         super().__init__(pos, model)
         self.pos = pos
         # The robot is assigned a randomly generated colour for visualisation
@@ -91,7 +90,7 @@ class Robot(Agent):
                                     self.model.grid.move_agent(self, tuple(self.path[self.path_step]))
                                     self.path_step = 1
                             else:
-                                print("Cell in robot", str(self.unique_id)+"'s", "path occupied")
+                                print("Cell", tuple(self.path[self.path_step]), "in robot", str(self.unique_id)+"'s", "path occupied")
                                 neighbours = []
                                 for cell in self.model.grid.iter_neighborhood(self.pos, False, False):
                                     neighbours.append(cell)
@@ -117,6 +116,14 @@ class Robot(Agent):
             elif self.path_step == len(self.path) - 1:
                 self.model.sampled[self.path[self.path_step][1], self.path[self.path_step][0]] = \
                     self.model.gaussian[self.path[self.path_step][0], self.path[self.path_step][1]]
+
+                # Check goal cell for SampledCell agent (i.e. already sampled)
+
+                for a in self.model.grid.get_cell_list_contents((self.path[self.path_step][0],
+                                                                 self.path[self.path_step][1])):
+                    if a.type == 2:
+                        self.model.grid.remove_agent(a)
+
                 # For the purpose of visualisation in MESA, the sampled cell is instantiated as an agent and placed on
                 # the grid to shade the cell according to the sampled value
                 agent = SampledCell(self.goal, self.model, self.model.gaussian[self.path[self.path_step][0],
@@ -145,6 +152,14 @@ class SampledCell(Agent):
         self.type = 1  # Agent type 1 is a sampled cell
 
 
+class UnsampledCell(SampledCell):
+    def __init__(self, pos, model):
+        super().__init__(pos, model)
+        self.pos = pos
+        self.color = "BlanchedAlmond"
+        self.type = 2  # Agent type 2 is an unsampled cell
+
+
 class SpatialSamplingModel(Model):
     def __init__(self, height=20, width=20, num_robots=2):
         self.height = height
@@ -155,6 +170,7 @@ class SpatialSamplingModel(Model):
         self.schedule = SimultaneousActivation(self)
         # The grid is multi-layered, and does not loop at the edges
         self.grid = MultiGrid(width, height, torus=False)
+
         # An underlying 2D Gaussian distribution is created for the robots to sample values from
         # At present this can only be a square matrix, hence only using the height
         self.gaussian = makeGaussian(height)
@@ -162,6 +178,11 @@ class SpatialSamplingModel(Model):
         self.step_num = 0
         self.num_goals = 0
         self.all_cells_assigned = False
+
+        # Place UnsampledCell agents for visualisation of unsampled cells
+        for x in range(self.width):
+            for y in range(self.height):
+                self.grid.place_agent(UnsampledCell(x, y), (x, y))
 
         # Place robots in grid at random locations, continuously re-assigning these when the location already contains
         # another robot
@@ -202,7 +223,7 @@ class SpatialSamplingModel(Model):
 
                 # If the current robot in the iteration has no goal, assign it one at an unsampled cell
                 if not agent.goal:
-                    goal_pos = self.grid.find_empty()
+                    goal_pos = (random.randrange(0, self.width), random.randrange(0, self.height))
                     if goal_pos is not None:
                         # Create a list of all other robots' goals to check for and reassign any duplicate goals
                         current_goal_cells = []
@@ -210,18 +231,20 @@ class SpatialSamplingModel(Model):
                             if a.type == 0 and a.goal is not None and a.unique_id != agent.unique_id and a.goal != []:
                                 current_goal_cells.append(a.goal)
                         goal_val_loop_runs = 0
+
+                        # Check goal cell for SampledCell agent (i.e. already sampled)
+                        goal_cell_already_sampled = True
+                        for a in self.grid.get_cell_list_contents(goal_pos):
+                            if a.type == 2:
+                                goal_cell_already_sampled = False
+
+                        if goal_cell_already_sampled:
+                            print("Goal cell already sampled")
+
                         # Ensure that the goal cell has not been sampled or assigned to another robot
-                        while self.sampled[goal_pos[1], goal_pos[0]] != -1 or \
-                                goal_pos in current_goal_cells:
+                        while self.sampled[goal_pos[1], goal_pos[0]] != -1 or goal_pos in current_goal_cells or\
+                                goal_cell_already_sampled:
                             goal_val_loop_runs += 1
-                            num_unsampled_cells = np.count_nonzero(self.sampled == -1)
-                            # Prevents assignment of goals when all remaining unsampled cells have already been assigned
-                            # as sampling goals to the robots
-                            if num_unsampled_cells <= len(self.robots):
-                                print(num_unsampled_cells, "unsampled cells remaining for", len(self.robots), "robots")
-                                print("Halting assignment of additional goal cells to robot", agent.unique_id)
-                                self.all_cells_assigned = True
-                                break
 
                             # For debugging, informs the user if the goal validation loop has run an excessive number of
                             # times and therefore may be stuck. Can be useful to set a breakpoint here if the loop gets
@@ -231,7 +254,38 @@ class SpatialSamplingModel(Model):
                                 print("Loop stuck?")
                             if agent.goal in current_goal_cells:
                                 print("Goal", agent.goal, "already assigned in", current_goal_cells)
-                            goal_pos = self.grid.find_empty()
+
+                            goal_pos = (random.randrange(0, self.width), random.randrange(0, self.height))
+
+                            num_unsampled_cells = np.count_nonzero(self.sampled == -1)
+                            if num_unsampled_cells == 0:
+                                break
+
+                            # Check goal cell for SampledCell agent (i.e. already sampled)
+                            goal_cell_already_sampled = True
+                            for a in self.grid.get_cell_list_contents(goal_pos):
+                                if a.type == 2:
+                                    goal_cell_already_sampled = False
+                            if goal_cell_already_sampled:
+                                continue
+
+                            # Prevents assignment of goals when all remaining unsampled cells have already been assigned
+                            # as sampling goals to the robots
+                            if num_unsampled_cells <= len(self.robots):
+                                print(num_unsampled_cells, "unsampled cells remaining for", len(self.robots), "robots")
+
+                                unsampled_cells = np.where(self.sampled == -1)
+                                unsampled_cells = set(zip(unsampled_cells[1], unsampled_cells[0]))
+                                print("Goal cells:", set(current_goal_cells), "len:", len(set(current_goal_cells)))
+                                print("Unsampled cells:", unsampled_cells, "len:", len(unsampled_cells))
+                                print("Goal cells are a subset of unsampled cells:",
+                                      set(current_goal_cells).issubset(unsampled_cells))
+                                if unsampled_cells == set(current_goal_cells):
+                                    print("All unsampled cells assigned")
+
+                                print("Halting assignment of additional goal cells to robot", agent.unique_id)
+                                self.all_cells_assigned = True
+                                break
                         # Finally assign the goal to the robot, after the goal has been validated, i.e. it is for an as
                         # yet unsampled cell, does not contain another robot, and is not already assigned to another
                         # robot
